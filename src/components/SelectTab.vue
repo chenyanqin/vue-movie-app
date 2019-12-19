43f0df8c95c16a9a4230ef3dae4e45c6c4db1855
chenyanqin <chenyanqin@itsmycar.cn>
2019-12-19 09:49:02 +0800

    fix: 时间切换筛选项初始化

--- src/components/SelectTab.vue
+++ src/components/SelectTab.vue
@@ -118,6 +118,7 @@ export default {
 		},
 		handleToTime(index) {
 			this.timeActiveIndex = index;
+			this.categoryActiveIndex = null;
 		},
 		handleToSearch() {
 			this.$router.push('/search');
